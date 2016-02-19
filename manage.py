5f4560fd3bd916e2defede04bec4216a84c37ea7
juice500ml <juice500ml@gmail.com>
2016-02-19 12:13:18 +0000

    added db manager

---
+++ manage.py
@@ -0,0 +1,9 @@
+from main import app
+from flask_script import Manager
+from flask_migrate import MigrateCommand
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
